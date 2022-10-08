985792c741075834ad572f54a133f951c2507517
Tamas Majer <tamas.majer@gmail.com>
2022-10-09 00:59:24 +0200

    fix: remove comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
-import react from "@astrojs/react"; // import { astroAuthComponents } from "@astro-auth/ui";
-// https://astro.build/config
-
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node(),
